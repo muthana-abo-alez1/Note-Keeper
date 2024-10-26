f7a72b5e1cd584e0b687f946eb61999dcf1e95b6
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-10-26 15:05:27 +0200

    add environment variable

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/noteApp');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
